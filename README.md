90c14f1af5c15275f443c58825cb48d3770e35f5
rani1405 <jdpatel029@gmail.com>
2024-12-15 10:25:37 -0500

    Junit tests

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SpringDataJPAExample
-This os sample project for spring CRUD operations
+This is sample project for spring CRUD operations
+1)Entity - 1:M relation
+2)Spring boot Exception Handler
+3)Junit
+  a)Unit testcases
--- src/main/java/com/example/springdatajpa/repository/EmployeeRepository.java
+++ src/main/java/com/example/springdatajpa/repository/EmployeeRepository.java
@@ -2,7 +2,6 @@ package com.example.springdatajpa.repository;
 
 import com.example.springdatajpa.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
